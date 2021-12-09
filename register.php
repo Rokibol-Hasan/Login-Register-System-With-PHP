4fb99f18b2461170dbda80e4acc183fed4574d49
Rokibol Hasan <rokibul.techdynobd@gmail.com>
2021-12-10 00:10:27 +0600

    2nd day

--- register.php
+++ register.php
@@ -2,14 +2,14 @@
     include "inc/header.php";
     include "lib/User.php";
     ?>
-    <?php
-    
+ <?php
+
     $user = new User();
-    if ($_SERVER['REQUEST_METHOD'=='POST') {
-        # code...
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])) {
+        $usrRegi = $user->userRegistration($_POST);
     }
-    
-    
+
+
     ?>
 
  <section class="userlist">
@@ -27,26 +27,33 @@
                      </div>
                      <div class="panel-body">
                          <div style="max-width: 600px; margin:0 auto;">
+
+                             <?php
+                                if (isset($usrRegi)) {
+                                    echo $usrRegi;
+                                }
+                                ?>
+
                              <form action="" method="POST">
                                  <div class="form-group">
                                      <label for="name">Name:</label>
-                                     <input type="text" id="name" name="name" class="form-control" required="">
+                                     <input type="text" id="name" name="name" class="form-control">
                                  </div>
                                  <div class="form-group">
                                      <label for="username">Username:</label>
-                                     <input type="text" id="username" name="username" class="form-control" required="">
+                                     <input type="text" id="username" name="username" class="form-control">
                                  </div>
                                  <div class="form-group">
                                      <label for="email">Email Address:</label>
-                                     <input type="text" id="email" name="email" class="form-control" required="">
+                                     <input type="text" id="email" name="email" class="form-control">
                                  </div>
                                  <div class="form-group">
                                      <label for="email">Password:</label>
-                                     <input type="password" id="password" name="password" class="form-control" required="">
+                                     <input type="password" id="password" name="password" class="form-control">
                                  </div>
-                                 <button type="submit" name="login" class="btn btn-success">Submit</button>
-
+                                 <button type="submit" name="register" class="btn btn-success">Submit</button>
                              </form>
+
                          </div>
                      </div>
 
